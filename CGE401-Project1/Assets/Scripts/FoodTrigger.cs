f363863d86b751d65bb51190d55175d34c085b18
mimi015 <mdavi5086@365.elmhurst.edu>
2025-09-23 21:53:20 -0500

    Panel opens on collision with food

---
+++ CGE401-Project1/Assets/Scripts/FoodTrigger.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FoodTrigger : MonoBehaviour
+{
+    public GameObject uiPanel;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            if (uiPanel != null)
+            {
+                uiPanel.SetActive(true);
+            }
+            else
+            {
+                uiPanel.SetActive(false);
+            }
+        }
+    }
+}
