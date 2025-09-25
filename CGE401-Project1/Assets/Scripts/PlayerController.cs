dc3f338f10d2c1ae495de685124bfc8d3a3bbc24
mimi015 <mdavi5086@365.elmhurst.edu>
2025-09-24 22:40:01 -0500

    Finished Player Animations

--- CGE401-Project1/Assets/Scripts/PlayerController.cs
+++ CGE401-Project1/Assets/Scripts/PlayerController.cs
@@ -10,14 +10,17 @@ public class PlayerController : MonoBehaviour
     public Transform groundCheck;
     public float groundCheckRadius = 0.2f;
 
-    private bool isGrounded;
+    bool isGrounded = false;
     private Rigidbody2D rb;
     private float horizontalInput;
+    private Animator animator;
     
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        
         if (groundCheck == null)
         {
             Debug.LogError("GroundCheck not assigned to the player controller!");
@@ -27,28 +30,42 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
         horizontalInput = Input.GetAxis("Horizontal");
-        rb.velocity = new Vector2(horizontalInput * moveSpeed, rb.velocity.y);
 
         if (Input.GetButtonDown("Jump") && isGrounded)
         {
             rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+            isGrounded = false;
+            animator.SetBool("isJumping", !isGrounded);
         }
     }
+
     void FixedUpdate()
     {
-       
-        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
+       rb.velocity = new Vector2(horizontalInput * moveSpeed, rb.velocity.y);
 
-        
-        if (horizontalInput > 0)
-        {
+       animator.SetFloat("xVelocity", Mathf.Abs(rb.velocity.x));
+
+       animator.SetFloat("yVelocity", rb.velocity.y);
+
+       isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
+
+
+       if (horizontalInput > 0)
+       {
             transform.rotation = Quaternion.Euler(0, 0, 0);
-        }
-        else if (horizontalInput < 0)
-        {
+       }
+       else if (horizontalInput < 0)
+       {
             transform.rotation = Quaternion.Euler(0, 180, 0);
-        }
+       }
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collison)
+    {
+           isGrounded = true;
+           animator.SetBool("isJumping", !isGrounded);
     }
 }
