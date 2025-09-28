d20d691d90a3adde99aef04ec582b69e0e7fccd0
mimi015 <mdavi5086@365.elmhurst.edu>
2025-09-28 04:25:45 -0500

    small updates on minigame3

---
+++ CGE401-Project1/Assets/Scripts/ClickableItem.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ClickableItem : MonoBehaviour, IPointerClickHandler
+{
+    private GameTimer3 gameTimer; // Reference to your timer script
+
+    void Start()
+    {
+        gameTimer = FindObjectOfType<GameTimer3>(); // Find the timer in the scene
+    }
+
+
+    // For 2D objects with Colliders and EventSystem, or UI elements, you might use:
+    public void OnPointerClick(PointerEventData eventData)
+    {
+       if (gameTimer != null && gameTimer.IsTimerRunning())
+       {
+            Debug.Log("Item Clicked before time ran out!");
+            // Perform action, e.g., destroy item, add score
+            Destroy(gameObject);
+       }
+       else
+       {
+            Debug.Log("Item clicked, but time has run out!");
+       }
+    }
+}
---
+++ CGE401-Project1/Assets/Scripts/GameTimer3.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameTimer3 : MonoBehaviour
+{
+    public float maxTime = 30f;
+    public Text timerText; // Assign in Inspector
+
+    private float timeRemaining;
+    private bool timerIsRunning = false;
+
+    void Start()
+    {
+        timeRemaining = maxTime;
+        timerIsRunning = true;
+    }
+
+    void Update()
+    {
+        if (timerIsRunning)
+        {
+            if (timeRemaining > 0)
+            {
+                timeRemaining -= Time.deltaTime;
+                timerText.text = "Time: " + Mathf.FloorToInt(timeRemaining).ToString();
+            }
+            else
+            {
+                timeRemaining = 0;
+                timerIsRunning = false;
+                timerText.text = "Time's Up!";
+                Debug.Log("Game Over - Time Ran Out!");
+                // Trigger game over logic here
+            }
+        }
+    }
+
+    public bool IsTimerRunning()
+    {
+        return timerIsRunning;
+    }
+}
